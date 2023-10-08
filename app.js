e88563a9b193ab85c1d85249ad44ed08ad60158a
Jakub Zając <jakub.zajac2611@gmail.com>
2023-10-08 10:37:56 +0200

    added animation to roll dice

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 const btnDice = document.querySelector(".button");
+const diceContainer = document.querySelector(".dice_container_One");
 
 function diceAction() {
-	let diceOneOne = document.querySelector(".dice_one_f1");
-	let diceOneTwo = document.querySelector(".dice_one_f2");
-	let diceOneThree = document.querySelector(".dice_one_f3");
-	let diceOneFour = document.querySelector(".dice_one_f4");
-	let diceOneFive = document.querySelector(".dice_one_f5");
-	let diceOneSix = document.querySelector(".dice_one_f6");
+	diceContainer.classList.add("hide");
+	diceContainer.style.animation = "flyDice 2s";
 
-	let diceOne = Math.floor(Math.random() * 6 + 1);
+	setTimeout(() => {
+		let diceOneOne = document.querySelector(".dice_one_f1");
+		let diceOneTwo = document.querySelector(".dice_one_f2");
+		let diceOneThree = document.querySelector(".dice_one_f3");
+		let diceOneFour = document.querySelector(".dice_one_f4");
+		let diceOneFive = document.querySelector(".dice_one_f5");
+		let diceOneSix = document.querySelector(".dice_one_f6");
 
-	diceOne === 1
-		? (diceOneOne.style.zIndex = "1")
-		: (diceOneOne.style.zIndex = "0");
-	diceOne === 2
-		? (diceOneTwo.style.zIndex = "1")
-		: (diceOneTwo.style.zIndex = "0");
-	diceOne === 3
-		? (diceOneThree.style.zIndex = "1")
-		: (diceOneThree.style.zIndex = "0");
-	diceOne === 4
-		? (diceOneFour.style.zIndex = "1")
-		: (diceOneFour.style.zIndex = "0");
-	diceOne === 5
-		? (diceOneFive.style.zIndex = "1")
-		: (diceOneFive.style.zIndex = "0");
-	diceOne === 6
-		? (diceOneSix.style.zIndex = "1")
-		: (diceOneSix.style.zIndex = "0");
+		let diceOne = Math.floor(Math.random() * 6 + 1);
+
+		diceContainer.style.animation = "none";
+
+		diceOneOne.style.zIndex = diceOne === 1 ? "1" : "0";
+		diceOneTwo.style.zIndex = diceOne === 2 ? "1" : "0";
+		diceOneThree.style.zIndex = diceOne === 3 ? "1" : "0";
+		diceOneFour.style.zIndex = diceOne === 4 ? "1" : "0";
+		diceOneFive.style.zIndex = diceOne === 5 ? "1" : "0";
+		diceOneSix.style.zIndex = diceOne === 6 ? "1" : "0";
+
+		diceContainer.classList.remove("hide");
+	}, 1000);
 }
 
 btnDice.addEventListener("click", diceAction);
