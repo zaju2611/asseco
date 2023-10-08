7d5c216ef8175163df887b23615cc121338c347d
Jakub Zając <jakub.zajac2611@gmail.com>
2023-10-08 12:45:41 +0200

    refactored code, added new images

--- app.js
+++ app.js
@@ -1,31 +1,106 @@
+/*
+
+Zadanie programistyczne 
+•	Należy stworzyć aplikację, która będzie symulowała grę planszową. 
+•	Parametrem wejściowym powinna być liczba wyrzuconych oczek klasyczną kostką sześcienną. 
+•	Plansza ma 20 pól. Pola 12 i 19 są polami specjalnymi. 
+•	Użytkownik rzucą kostką raz za razem. Przesuwa się po planszy zgodnie z wyrzuconą liczbą oczek. Gra kończy się w przypadku wejścia na pole o numerze 12 (gra kończy się porażką), bądź osiągnięcia pola 20 (wygrana). 
+•	Jeżeli gracz po rzucie przekroczy pole 20, to cofa się o tyle oczek, o ile przekroczył pole 20 (np. stoi na 18 polu, jeżeli wyrzuci 5 oczek to cofa się do pola o numerze 17). 
+•	W przypadku trafienia na pole o numerze 19, gracz przesuwa się na pole o numerze 11. 
+•	Jako wynik użytkownik powinien dostać informację czy gra zakończyła się sukcesem, informację o liczbie rzutów oraz średnią liczbę wyrzuconych oczek. 
+•	Należy przygotować aplikację tak, aby można było dołożyć obsługę kolejnych pól specjalnych. 
+•	Przebieg gry można przedstawić za pomocą GUI lub informacji logowanych w konsoli 
+ 
+ 
+•	Założenia: 
+o	Język programowania: JavaScript 
+o	Można użyć dowolnych bibliotek zewnętrznych 
+o	Termin realizacji: do 13 października 2023 r., godzina 23:59 (piątek) 
+o	Forma dostarczenia dowolna: całość na mail / zewnętrzne repo / github / etc 
+ 
+ */
 const btnDice = document.querySelector(".button");
 const diceContainer = document.querySelector(".dice_container_One");
+const fields = document.querySelectorAll(".boardPleace");
 
-function diceAction() {
+const boardSize = 20;
+const specialFields = {
+	12: -1,
+	19: 11,
+};
+let currentPosition = 1;
+let previousPosition = 1;
+let totalRolls = 0;
+let totalPoints = 0;
+let isGameInProgress = true;
+
+const diceFaces = [
+	"dice_one_f1",
+	"dice_one_f2",
+	"dice_one_f3",
+	"dice_one_f4",
+	"dice_one_f5",
+	"dice_one_f6",
+];
+
+const rollDice = () => {
+	if (!isGameInProgress) return;
+
+	playTurn();
+};
+
+function rollAnimation() {
 	diceContainer.classList.add("hide");
 	diceContainer.style.animation = "flyDice 2s";
 
 	setTimeout(() => {
-		let diceOneOne = document.querySelector(".dice_one_f1");
-		let diceOneTwo = document.querySelector(".dice_one_f2");
-		let diceOneThree = document.querySelector(".dice_one_f3");
-		let diceOneFour = document.querySelector(".dice_one_f4");
-		let diceOneFive = document.querySelector(".dice_one_f5");
-		let diceOneSix = document.querySelector(".dice_one_f6");
+		const diceOne = Math.floor(Math.random() * 6 + 1);
 
-		let diceOne = Math.floor(Math.random() * 6 + 1);
+		totalRolls++;
+		totalPoints += diceOne;
 
 		diceContainer.style.animation = "none";
 
-		diceOneOne.style.zIndex = diceOne === 1 ? "1" : "0";
-		diceOneTwo.style.zIndex = diceOne === 2 ? "1" : "0";
-		diceOneThree.style.zIndex = diceOne === 3 ? "1" : "0";
-		diceOneFour.style.zIndex = diceOne === 4 ? "1" : "0";
-		diceOneFive.style.zIndex = diceOne === 5 ? "1" : "0";
-		diceOneSix.style.zIndex = diceOne === 6 ? "1" : "0";
+		diceFaces.forEach((face, index) => {
+			const diceElement = document.querySelector(`.${face}`);
+			diceElement.style.zIndex = diceOne === index + 1 ? "1" : "0";
+		});
 
 		diceContainer.classList.remove("hide");
+		previousPosition = currentPosition;
+
+		if (specialFields[currentPosition]) {
+			currentPosition = specialFields[currentPosition];
+			console.log(`Gracz na polu specjalnym ${currentPosition}`);
+		} else {
+			currentPosition += diceOne;
+			if (currentPosition === 19) {
+				currentPosition = 11;
+			}
+		}
+
+		if (currentPosition > boardSize) {
+			currentPosition = boardSize - (currentPosition - boardSize);
+		}
+		console.log(`Rzut: ${diceOne}, Pozycja gracza: ${currentPosition}`);
+		fields[currentPosition - 1].classList.add("active");
+		fields[previousPosition - 1].classList.remove("active");
+
+		if (currentPosition === 20 || currentPosition === -1) {
+			endGame();
+		}
 	}, 1000);
 }
 
-btnDice.addEventListener("click", diceAction);
+function endGame() {
+	isGameInProgress = false;
+	console.log(
+		`${
+			currentPosition === 20 ? "Gratulacje, wygrałeś!" : "Gracz przegrał."
+		} Liczba rzutów: ${totalRolls}, Średnia wartość rzutu: ${
+			totalPoints / totalRolls
+		}`
+	);
+}
+
+btnDice.addEventListener("click", rollAnimation);
--- game.js
+++ game.js
@@ -1,24 +1,4 @@
-/*
 
-Zadanie programistyczne 
-•	Należy stworzyć aplikację, która będzie symulowała grę planszową. 
-•	Parametrem wejściowym powinna być liczba wyrzuconych oczek klasyczną kostką sześcienną. 
-•	Plansza ma 20 pól. Pola 12 i 19 są polami specjalnymi. 
-•	Użytkownik rzucą kostką raz za razem. Przesuwa się po planszy zgodnie z wyrzuconą liczbą oczek. Gra kończy się w przypadku wejścia na pole o numerze 12 (gra kończy się porażką), bądź osiągnięcia pola 20 (wygrana). 
-•	Jeżeli gracz po rzucie przekroczy pole 20, to cofa się o tyle oczek, o ile przekroczył pole 20 (np. stoi na 18 polu, jeżeli wyrzuci 5 oczek to cofa się do pola o numerze 17). 
-•	W przypadku trafienia na pole o numerze 19, gracz przesuwa się na pole o numerze 11. 
-•	Jako wynik użytkownik powinien dostać informację czy gra zakończyła się sukcesem, informację o liczbie rzutów oraz średnią liczbę wyrzuconych oczek. 
-•	Należy przygotować aplikację tak, aby można było dołożyć obsługę kolejnych pól specjalnych. 
-•	Przebieg gry można przedstawić za pomocą GUI lub informacji logowanych w konsoli 
- 
- 
-•	Założenia: 
-o	Język programowania: JavaScript 
-o	Można użyć dowolnych bibliotek zewnętrznych 
-o	Termin realizacji: do 13 października 2023 r., godzina 23:59 (piątek) 
-o	Forma dostarczenia dowolna: całość na mail / zewnętrzne repo / github / etc 
- 
- */
 const dice = document.querySelector(".dice");
 
 const diceRoll = () => {
@@ -47,28 +27,7 @@ const playTurn = () => {
 	totalRolls++;
 	totalPoints += diceValue;
 
-	if (specialFields[currentPosition]) {
-		currentPosition = specialFields[currentPosition];
-		console.log(`Gracz na polu specjalnym ${currentPosition}`);
-	} else {
-		currentPosition += diceValue;
-	}
-
-	if (currentPosition > boardSize) {
-		currentPosition = boardSize - (currentPosition - boardSize);
-	}
-	console.log(`Rzut: ${diceValue}, Pozycja gracza: ${currentPosition}`);
-
-	if (currentPosition === 20 || currentPosition === -1) {
-		isGameInProgress = false;
-		console.log(
-			`${
-				currentPosition === 20 ? "Gratulacje, wygrałeś!" : "Gracz przegrał."
-			} Liczba rzutów: ${totalRolls}, Średnia wartość rzutu: ${
-				totalPoints / totalRolls
-			}`
-		);
-	}
+	
 };
 
 dice.addEventListener("click", game);
