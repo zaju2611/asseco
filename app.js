dc650854f32332422bfcfe11d6c460d245222af2
Jakub Zając <jakub.zajac2611@gmail.com>
2023-10-08 10:21:12 +0200

    added dice to game

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-function rollDice() {
-	const dice = [...document.querySelectorAll(".die-list")];
-	dice.forEach((die) => {
-		toggleClasses(die);
-		die.dataset.roll = getRandomNumber(1, 6);
-	});
+const btnDice = document.querySelector(".button");
+
+function diceAction() {
+	let diceOneOne = document.querySelector(".dice_one_f1");
+	let diceOneTwo = document.querySelector(".dice_one_f2");
+	let diceOneThree = document.querySelector(".dice_one_f3");
+	let diceOneFour = document.querySelector(".dice_one_f4");
+	let diceOneFive = document.querySelector(".dice_one_f5");
+	let diceOneSix = document.querySelector(".dice_one_f6");
+
+	let diceOne = Math.floor(Math.random() * 6 + 1);
+
+	diceOne === 1
+		? (diceOneOne.style.zIndex = "1")
+		: (diceOneOne.style.zIndex = "0");
+	diceOne === 2
+		? (diceOneTwo.style.zIndex = "1")
+		: (diceOneTwo.style.zIndex = "0");
+	diceOne === 3
+		? (diceOneThree.style.zIndex = "1")
+		: (diceOneThree.style.zIndex = "0");
+	diceOne === 4
+		? (diceOneFour.style.zIndex = "1")
+		: (diceOneFour.style.zIndex = "0");
+	diceOne === 5
+		? (diceOneFive.style.zIndex = "1")
+		: (diceOneFive.style.zIndex = "0");
+	diceOne === 6
+		? (diceOneSix.style.zIndex = "1")
+		: (diceOneSix.style.zIndex = "0");
 }
 
-function toggleClasses(die) {
-	die.classList.toggle("odd-roll");
-	die.classList.toggle("even-roll");
-}
-
-function getRandomNumber(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-document.getElementById("roll-button").addEventListener("click", rollDice);
+btnDice.addEventListener("click", diceAction);
